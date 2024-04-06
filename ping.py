809aee404a8ced03505f752c405b35e7efd73629
ktyptyt22812 <165475304+ktyptyt22812@users.noreply.github.com>
2024-04-06 16:21:21 +0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,7 +1,7 @@
 ping = input('')
-if ping == 'ping':
-  print('pong!')
-else:
-  print('no pong..')
 while True:
-  print('1111')
+  if ping == 'ping':
+    print('pong!')
+  else:
+    print('no pong..')
+
