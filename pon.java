9526484d0adcb9d9852f9337cb7a21a24d2baf51
ktyptyt22812 <165475304+ktyptyt22812@users.noreply.github.com>
2024-04-08 09:56:11 +0600

    нуфы

--- pon.java
+++ pon.java
@@ -2,28 +2,8 @@ public class Helloworld {
     public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
-        System.out.println(a - b);
+        System.out.println(a);
+        System.out.println(b);
         System.out.println(a - b);
     }
 }
