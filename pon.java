8bf5416c22dff71f6954bdab764fc0d55c3d0fe8
ktyptyt22812 <165475304+ktyptyt22812@users.noreply.github.com>
2024-04-15 10:01:25 +0600

    Update pon.java

--- pon.java
+++ pon.java
@@ -1,9 +1,17 @@
 public class Helloworld {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
+        int a = 1488;
+        int b = 1183;
+        int c = 128;
+        int d = 64;
+        int e = 12;
+        int g = 48;
         System.out.println(a);
         System.out.println(b);
-        System.out.println(a - b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(a - b + c - d - a + e - g);
     }
 }
